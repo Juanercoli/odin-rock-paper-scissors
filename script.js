afc636e50c171f25b182a8f0febffd02613b27ed
Juan Ercoli <ercoli.juan@alu.ing.unlp.edu.ar>
2023-12-26 02:23:51 -0300

    finish rps-ui

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
+// Possible options for playing rps
 options = ["Rock", "Paper", "Scissors"];
 
-game();
+let optionsContainer = document.querySelector("#container-options");
+// Score
+let playerScore = 0;
+let computerScore = 0;
+
+document.querySelector("#score-player").textContent = playerScore;
+document.querySelector("#score-computer").textContent = computerScore;
+
+options.forEach((option) => {
+  // For every option create a button
+  let button = document.createElement("button");
+  button.textContent = option;
+  button.addEventListener("click", () => {
+    // If button is clicked then a round is played
+    let computerChoice = getComputerChoice();
+    result = playRound(option, computerChoice);
+    // Update score
+    if (result === "player") {
+      playerScore++;
+      document.querySelector("#score-player").textContent = playerScore;
+    } else if (result === "computer") {
+      computerScore++;
+      document.querySelector("#score-computer").textContent = computerScore;
+    }
+    // Shows result on messages container
+    let message = document.createElement("p");
+    message.textContent = getResultMessage(result, option, computerChoice);
+    document.querySelector("#container-messages").append(message);
+    // Check winner
+    if (playerScore === 5 || computerScore === 5) {
+      playerScore === 5 ? alert("Player wins!") : alert("Computer wins!");
+      playerScore = 0;
+      computerScore = 0;
+      document.querySelector("#score-player").textContent = playerScore;
+      document.querySelector("#score-computer").textContent = computerScore;
+      document.querySelectorAll("p").forEach(p => p.remove());
+    }
+  });
+  // Add button to options container
+  optionsContainer.append(button);
+});
+
+function getResultMessage(result, playerChoice, computerChoice) {
+  return result === "tie"
+    ? `It's a tie!`
+    : result === "computer"
+    ? `You lose! ${computerChoice.toLowerCase()} beats ${playerChoice.toLowerCase()}`
+    : `You win! ${playerChoice.toLowerCase()} beats ${computerChoice.toLowerCase()}`;
+}
 
 function getComputerChoice() {
   return options[Math.floor(Math.random() * options.length)];
@@ -11,7 +60,6 @@ function playRound(playerSelection, computerSelection) {
   let areEqual =
     playerSelection.toLowerCase() === computerSelection.toLowerCase();
   if (areEqual) {
-    console.log(`It's a tie!`);
     return "tie";
   }
 
@@ -21,44 +69,8 @@ function playRound(playerSelection, computerSelection) {
     ].toLowerCase();
 
   if (playerSelection.toLowerCase() === optionToWin) {
-    console.log(
-      `You win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`
-    );
     return "player";
   }
 
-  console.log(
-    `You lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`
-  );
   return "computer";
-}
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let result = "tie";
-    while (result === "tie") {
-      let playerSelection = prompt("Rock, Paper or Scissors");
-      // Checks if input is valid
-      while (
-        !options.some(
-          (option) => option.toLowerCase() === playerSelection.toLowerCase()
-        )
-      ) {
-        playerSelection = prompt("Rock, Paper or Scissors");
-      }
-      // Play round
-      result = playRound(playerSelection, getComputerChoice());
-    }
-
-    // Update score
-    if (result === "player") playerScore++;
-    else computerScore++;
-  }
-
-  // Get the winner
-  if (playerScore < computerScore) console.log("Player loses!");
-  else console.log("Player wins!");
 }
